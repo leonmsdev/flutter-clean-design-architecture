e42dad99f6718cca7649c49989f92a92d17b462e
leonmsdev <leonms.dev@gmail.com>
2023-02-18 15:18:26 +0100

    feat(init):initialise flutter app

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //Sould be wrapped with Bloc Widget.
+          const Text('I am an advice from the api'),
+          TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.api),
+              label: const Text('Start Api request '))
+        ],
+      ),
+    );
+  }
+}
